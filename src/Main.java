83c65b9916885a47d2cdb393343378f659579eea
IvanMikhailiukovich <138164038+IvanMikhailiukovich@users.noreply.github.com>
2025-01-28 01:16:38 +0300

    Update Main.java
    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! - 1/1");
+        System.out.println("изменить в удалённом репозитории любой файл двумя способами, сделав коммит: ");
+        System.out.println("изменить строки кода, которые затрагивали для изменений в предыдущем пункте;");
+        System.out.println("вписать новые строки кода, которые не затрагивают старые;");
     }
 }
